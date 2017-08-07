dc6b133747107891cde00326ded54fff7d0817ed
boom <459491643@qq.com>
2017-08-07 08:30:59 +0800

    add pause and play

--- main.js
+++ main.js
@@ -6,18 +6,19 @@ require.config({
 })
 
 require(
-['zrender', 'shape/Circle', 'tool/color', 'shape/Image', 'shape/Rectangle'],
+['zrender', 'shape/Image'],
 function (zrender) {
   const zr = zrender.init(document.getElementById('main'))
   const ImageShape = require('shape/Image')
   const height = Math.ceil(zr.getHeight())
   const zrWidth = Math.ceil(zr.getWidth())
   const frameNum = 30
+  let imgTimer = null
   let image = null
-  const RectangleShape = require('shape/Rectangle')
 
   getImgInfo(height, './images/1.jpg')
   .then(({width, imgWidth, imgHeight}) => {
+    let flag = false
     image = new ImageShape({
       style: {
         image: './images/1.jpg',
@@ -32,22 +33,28 @@ function (zrender) {
       },
       hoverable: false,
       clickable: true,
-      onclick: function () {
-        console.log(1)
+      onclick () {
+        if (!flag) clearInterval(imgTimer)
+        else startTimer({width, imgWidth, imgHeight})
+        flag = !flag
       }
     })
     zr.addShape(image)
-    zr.render(function () {
-      const imgTimer = setInterval(() => {
-        image.style.sx += 2
-        if (image.style.sx >= imgWidth - imgWidth / width * zrWidth) clearInterval(imgTimer)
-        zr.modShape(image.id, image)
-        zr.refresh()
-      }, 1e3 / frameNum)
+    zr.render(() => {
+      startTimer({width, imgWidth, imgHeight})
     })
   })
   window.addEventListener('resize', () => {
     zr.resize()
   })
+
+  function startTimer ({width, imgWidth, imgHeight}) {
+    imgTimer = setInterval(() => {
+      image.style.sx += 2
+      if (image.style.sx >= imgWidth - imgWidth / width * zrWidth) clearInterval(imgTimer)
+      zr.modShape(image.id, image)
+      zr.refresh()
+    }, 1e3 / frameNum)
+  }
 }
 )
